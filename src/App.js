6fd8957c08446fe1ec8c9f6f025e2c3f0ae3998d
dsl2022 <2015rpro@gmail.com>
2023-07-24 22:37:30 -0400

    updated

--- src/App.js
+++ src/App.js
@@ -2,27 +2,21 @@ import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function handleMagic(message,setMagic){
-  setMagic(message)
-  // console.log("magic is cool",message)
-}
-
-function MyButton({name,disabled,message,setMagic}){
-  return <div>
-    <button onClick={()=>handleMagic(message,setMagic)} disabled={disabled}>{name}</button>
-  </div>
-}
-
-
 function App() {
-  const [magic, setMagic] =useState("magic")
-  console.log("state test",magic)
+  const [count, setCount] =useState(0)
+  const increaseCount=()=>{
+    setCount(count+1)
+  }
+  
+  const decreaseCount=()=>{
+    setCount(count-1)
+  }
   return (    
     <div className="App">
-      <h1>React is cool</h1>
-      <MyButton name="Magic" disabled={false} message="light magic" setMagic={setMagic} />
-      {/* <MyButton name="Dark Magic" disabled={true} message="test" setMagic={setMagic}/> */}
-      <img src="https://cdn.britannica.com/16/234216-050-C66F8665/beagle-hound-dog.jpg" />
+      <h1>{count}</h1>
+      <button onClick={increaseCount} >+</button>
+      <button onClick={decreaseCount} >-</button>
+      
     </div>
     
   );
