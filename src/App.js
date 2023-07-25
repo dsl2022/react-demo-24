02606ae99902a6a5357ebd80707e7720107994ea
dsl2022 <2015rpro@gmail.com>
2023-07-24 21:40:13 -0400

    updated app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
+import { Fragment } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function handleMagic(message){
+  console.log("magic is cool",message)
+}
+
+function MyButton({name,disabled,message}){
+  return <div>
+    <button onClick={handleMagic(message)} disabled={disabled}>{name}</button>
+  </div>
+}
+
+
 function App() {
-  return (
+  return (    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React is cool</h1>
+      <MyButton name="Magic" disabled={false} message="test" />
+      <MyButton name="Dark Magic" disabled={true} message="test" />
+      <img src="https://cdn.britannica.com/16/234216-050-C66F8665/beagle-hound-dog.jpg" />
     </div>
+    
   );
 }
 
