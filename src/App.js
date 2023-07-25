5007ca043c2826c963ee48ddc3d7307bcffdf49f
dsl2022 <2015rpro@gmail.com>
2023-07-24 21:56:52 -0400

    updated state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import { Fragment } from 'react';
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function handleMagic(message){
-  console.log("magic is cool",message)
+function handleMagic(message,setMagic){
+  setMagic(message)
+  // console.log("magic is cool",message)
 }
 
-function MyButton({name,disabled,message}){
+function MyButton({name,disabled,message,setMagic}){
   return <div>
-    <button onClick={handleMagic(message)} disabled={disabled}>{name}</button>
+    <button onClick={()=>handleMagic(message,setMagic)} disabled={disabled}>{name}</button>
   </div>
 }
 
 
 function App() {
+  const [magic, setMagic] =useState("magic")
+  console.log("state test",magic)
   return (    
     <div className="App">
       <h1>React is cool</h1>
-      <MyButton name="Magic" disabled={false} message="test" />
-      <MyButton name="Dark Magic" disabled={true} message="test" />
+      <MyButton name="Magic" disabled={false} message="light magic" setMagic={setMagic} />
+      {/* <MyButton name="Dark Magic" disabled={true} message="test" setMagic={setMagic}/> */}
       <img src="https://cdn.britannica.com/16/234216-050-C66F8665/beagle-hound-dog.jpg" />
     </div>
     
